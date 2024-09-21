8448796a7c9bfb7cd6ce3e26b1ef9dffb5c162b2
chad-broderick0693 <c.broderick0693@gmail.com>
2024-09-21 19:06:35 -0400

    chore: Install and set up TresJS

--- personal-portfolio/src/views/HomeView.vue
+++ personal-portfolio/src/views/HomeView.vue
@@ -1,5 +1,18 @@
-<script setup></script>
+<script setup>
+import { OrbitControls } from '@tresjs/cientos'
+import { TresCanvas } from '@tresjs/core'
+</script>
 
 <template>
-  <main></main>
+  <main>
+    <TresCanvas window-size>
+      <TresPerspectiveCamera :position="[3, 3, 3]" :fov="45" :look-at="[0, 0, 0]" />
+      <TresMesh>
+        <TresTorusGeometry :args="[1, 0.5, 16, 32]" />
+        <TresMeshBasicMaterial color="orange" />
+      </TresMesh>
+      <TresAmbientLight :intensity="1" />
+      <OrbitControls />
+    </TresCanvas>
+  </main>
 </template>
